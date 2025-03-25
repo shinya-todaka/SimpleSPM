18755f260c770ee3c82fd66531166bbd073ff042
shinya-todaka <tdk0105.strawberry@gmail.com>
2025-03-25 21:54:19 +0900

    fix

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "SimpleSPM",
-    products: .library(name: "SimpleSPM", targets: ["SimpleSPM"]),
+    products: [.library(name: "SimpleSPM", targets: ["SimpleSPM"])],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
